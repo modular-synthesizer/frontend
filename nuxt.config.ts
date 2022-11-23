5a26c5bc90c0a4a4bfc38812b80f4770a6c47f34
Vincent <courtois.vincent@outlook.com>
2022-11-23 12:50:06 +0100

    Fix config for production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: false,
   css: [
     'vuetify/lib/styles/main.sass',
     'mdi/css/materialdesignicons.min.css',
