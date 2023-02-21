69a851f46cb19f4e08a1f4f29da1589d59c857d8
Vincent <courtois.vincent@outlook.com>
2023-02-21 11:01:03 +0100

    feat(Proxy): add more traces in the proxy to continue incremental testings

--- server/routes/proxy/[...].ts
+++ server/routes/proxy/[...].ts
@@ -1,1 +1,10 @@
-export default defineEventHandler((event) => ({foo: event.path}))
+export default defineEventHandler(async (event) => {
+
+  const method = event.node.req.method;
+  const url = `${process.env.API_URL}${event.path?.replace("/proxy", "")}`;
+  const params = getQuery(event) || {};
+  const body = await readBody(event);
+  const headers = getHeaders(event);
+
+  return { body, method, url, params, headers }
+})
