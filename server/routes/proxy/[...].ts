e9eb568f719b3b9785fdaa9457c4a14004ef8f76
Vincent <courtois.vincent@outlook.com>
2023-02-21 11:10:55 +0100

    Simplify the proxy route to see what parameter is problematic

--- server/routes/proxy/[...].ts
+++ server/routes/proxy/[...].ts
@@ -1,10 +1,6 @@
 export default defineEventHandler(async (event) => {
-
-  const method = event.node.req.method;
-  const url = `${process.env.API_URL}${event.path?.replace("/proxy", "")}`;
-  const params = getQuery(event) || {};
-  const body = await readBody(event);
-  const headers = getHeaders(event);
-
-  return { body, method, url, params, headers }
+  return {
+    method: event.node.req.method,
+    url: `${process.env.API_URL}${event.path?.replace("/proxy", "")}`
+  }
 })
