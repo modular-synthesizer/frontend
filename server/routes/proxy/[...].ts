3a1f9d5a1a877a233bb7c7d579cad2b96dae4309
Vincent <courtois.vincent@outlook.com>
2023-02-21 11:19:01 +0100

    Add the params to the proxy route

--- server/routes/proxy/[...].ts
+++ server/routes/proxy/[...].ts
@@ -1,6 +1,7 @@
 export default defineEventHandler(async (event) => {
   return {
     method: event.node.req.method,
-    url: `${process.env.API_URL}${event.path?.replace("/proxy", "")}`
+    url: `${process.env.API_URL}${event.path?.replace("/proxy", "")}`,
+    params: getQuery(event),
   }
 })
