4be554acd176e317f5a1497ca5fb7d5e29499698
Vincent <courtois.vincent@outlook.com>
2023-02-21 11:36:31 +0100

    Add the body to the proxy

--- server/routes/proxy/[...].ts
+++ server/routes/proxy/[...].ts
@@ -1,7 +1,11 @@
 export default defineEventHandler(async (event) => {
+
+  const rawBody = readBody(event);
+
   return {
     method: event.node.req.method,
     url: `${process.env.API_URL}${event.path?.replace("/proxy", "")}`,
     params: getQuery(event),
+    body: rawBody,
   }
 })
