848bb8fe9c20791b252346b05315e14cc3458c4f
Vincent <courtois.vincent@outlook.com>
2023-04-11 17:02:34 +0200

    Clean the code of the page a bit

--- pages/admin/[tab].vue
+++ pages/admin/[tab].vue
@@ -1,36 +1,27 @@
 <template>
-  <v-container>
-    <v-navigation-drawer expand-on-hover rail color="secondary">
-      <v-list density="compact" nav>
-        <v-list-item v-for="item in items" :prepend-icon="item.icon" :value="item.name" @click="display(item.name)">
-          {{ item.name }}
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-container>
-      <v-row>
-        <v-col cols="8" offset="2">
-          <v-window v-model="tab">
-            <v-window-item v-for="tab in items" :value="tab.name">
-              <component :is="tab.component" />
-            </v-window-item>
-          </v-window>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-container>
+  <h1 class="hidden-xs">test</h1>
+  <v-navigation-drawer expand-on-hover rail color="secondary">
+    <v-list density="compact" nav>
+      <v-list-item v-for="item in items" :prepend-icon="item.icon" :value="item.name" @click="() => (tab = item.name)">
+        {{ item.name }}
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+  <v-row no-gutter>
+    <v-col sm="10" cols="12" offset-sm="1" offset="0" md="8" offset-md="2">
+      <v-window v-model="tab">
+        <v-window-item v-for="tab in items" :value="tab.name">
+          <component :is="tab.component" />
+        </v-window-item>
+      </v-window>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
-import Categories from '~~/components/admin/categories.vue';
-import Parameters from '~~/components/admin/parameters.vue';
-import Applications from '~~/components/admin/applications.vue';
-import AdminRights from '~~/components/admin/rights.vue';
-import Generators from "~~/components/admin/generators.vue"
 import { resolveComponent } from 'vue';
 
 export default {
-  components: { Applications, Categories, Generators, Parameters, AdminRights },
   data: () => ({
     tab: 'groups',
     items: [
@@ -42,10 +33,5 @@ export default {
       {name: "generators", icon: "mdi-factory", component: resolveComponent('admin-generators')},
     ]
   }),
-  methods: {
-    display(name: string) {
-      this.tab = name;
-    }
-  }
-}
+};
 </script>
