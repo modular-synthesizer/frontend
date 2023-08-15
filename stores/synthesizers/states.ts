1ee1310afe74bee5a3faf03683cb559e1a9ce978
Vincent <courtois.vincent@outlook.com>
2023-08-16 00:05:11 +0200

    Add a store to manage the different synthesizers states

---
+++ stores/synthesizers/states.ts
@@ -0,0 +1,81 @@
+import { defineStore } from "pinia";
+
+export enum SynthState {
+  // The user is currently creating a link
+  CREATING_LINK = 'CREATING_LINK',
+  // The user is currently dragging a node
+  DRAGGING_MODULE = 'DRAGGING_MODULE',
+  // The user is currently editing a parameter
+  EDITING_PARAMETER = 'EDITING_PARAMETER',
+  // When creating a link, a user is magnetizing it to the end port
+  MAGNETIZING = 'MAGNETIZING',
+  // The synthesizer actually does nothing
+  NONE = 'NONE',
+  // The user is currently hovering an already created link
+  HOVERING_LINK = 'HOVERING_LINK',
+  // The user is currently hovering a module
+  HOVERING_MODULE = 'HOVERING_MODULE',
+  // The user is currently moving the whole synthesizer
+  PANNING = 'PANNING',
+  // The user is zooming in or out on the synthesizer
+  ZOOMING = 'ZOOMING',
+}
+
+interface Payload {
+  current: SynthState;
+  next: SynthState;
+  blocked: Boolean;
+}
+
+type BlockMapping = {
+  [key in SynthState]: Boolean;
+}
+
+/**
+ * This constant indicates which states are blocking. A blocking state forbids the application
+ * from being able to pass to another state before the store has been manually unblocked.
+ */
+const isBlocking: BlockMapping = {
+  [SynthState.CREATING_LINK]: true,
+  [SynthState.DRAGGING_MODULE]: true,
+  [SynthState.EDITING_PARAMETER]: true,
+  [SynthState.MAGNETIZING]: false,
+  [SynthState.NONE]: false,
+  [SynthState.HOVERING_LINK]: false,
+  [SynthState.HOVERING_MODULE]: false,
+  [SynthState.PANNING]: true,
+  [SynthState.ZOOMING]: true
+}
+
+/**
+ * This store holds the logic for the states a synthesizer can be in, and what state can be safely
+ * transitioned to another. It furthermore says which state is "blocking" (a blocking state is a state
+ * from which no new state can be set before it unblocks).
+ */
+export const useStates = defineStore("states", {
+  state(): Payload {
+    return {
+      current: SynthState.NONE,
+      next: SynthState.NONE,
+      blocked: false,
+    }
+  },
+  actions: {
+    setState(state: SynthState): void {
+      this.next = state;
+      if (!this.blocked) this.processNext();
+    },
+    unclock(): void {
+      if (this.next !== SynthState.NONE) this.processNext();
+      this.blocked = false;
+    },
+    is(state: SynthState): Boolean {
+      return this.current === state;
+    },
+    processNext(): void {
+      this.current = this.next;
+      this.next = SynthState.NONE;
+      if (isBlocking[this.current]) this.blocked = true;
+    },
+  },
+});
