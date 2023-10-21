b707301a3d1b6b7c05682d534733c2d5ba33808e
Vincent <courtois.vincent@outlook.com>
2023-10-21 12:05:35 +0200

    Delegate all the management of the parameter selection to the new composable

--- composables/parameters/selectParam.ts
+++ composables/parameters/selectParam.ts
@@ -1,2 +1,16 @@
+import Parameter from '~~/lib/wrappers/Parameter';
+
 // This composable holds the logic to know which parameter is currently being edited.
 // It does not handle the edition itself, the value or the coordinates.
+
+export let selectedParameter: Parameter|null = null;
+
+export function selectParameter(parameter: Parameter) {
+  selectedParameter = parameter;
+  console.log(selectedParameter)
+}
+
+export function unselectParameter() {
+  selectedParameter = null;
+  console.log(selectedParameter)
+}
--- stores/mods/parameters.ts
+++ stores/mods/parameters.ts
@@ -9,13 +9,11 @@ import Synthesizer from "~~/lib/wrappers/Synthesizer";
 const { EDITING_PARAMETER } = SynthState;
 
 interface Payload {
-  parameter: null | Parameter;
   yOrigin: number;
 }
 
 export const useParameters = defineStore('parameters', {
   state: (): Payload => ({
-    parameter: null,
     yOrigin: 0
   }),
   getters: {
@@ -31,26 +29,26 @@ export const useParameters = defineStore('parameters', {
       useContexts().hide();
       useStates().unblock();
       if (this.synth.isReadonly(this.username)) return;
-      this.parameter = parameter;
+      selectParameter(parameter);
       this.yOrigin = $event.clientY;
       useStates().setState(EDITING_PARAMETER);
       sendParamEvent('startEdit', parameter);
     },
     moveParameterSetting(_x: number, y: number) {
-      if (this.parameter === null) return;
+      if (selectedParameter === null) return;
 
       const delta = y - this.yOrigin;
       if (Math.abs(delta) >= 5) {
-        this.parameter.moveValue(- (delta / 5) * this.parameter.step);
+        selectedParameter.moveValue(- (delta / 5) * selectedParameter.step);
         this.yOrigin = y;
       }
     },
     endParameterSetting() {
-      if (this.parameter === null) return;
+      if (selectedParameter === null) return;
       useStates().unblock();
-      this.saveParameter(this.parameter as Parameter);
-      sendParamEvent('endEdit', this.parameter as Parameter);
-      this.parameter = null;
+      this.saveParameter(selectedParameter as Parameter);
+      sendParamEvent('endEdit', selectedParameter as Parameter);
+      unselectParameter();
     },
     async saveParameter(param: Parameter): Promise<any> {
       const auth_token: string = useAuthentication().session.token;
