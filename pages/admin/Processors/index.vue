2a06fc12f3268b7e996d206e45f991e5a08f06a2
Vincent <courtois.vincent@outlook.com>
2023-11-12 15:14:11 +0100

    Change the list of processors

--- pages/admin/Processors/index.vue
+++ pages/admin/Processors/index.vue
@@ -2,24 +2,17 @@
   <v-container>
     <v-row>
       <v-col cols="6" offset="3">
-        <v-btn to="/admin/processors/new">Créer nouveau</v-btn>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="6" offset="3">
         <v-table density="compact">
           <thead>
             <tr>
               <th>UUID</th>
-              <th>Name</th>
-              <th>Class name</th>
+              <th>URL</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="processor in processors">
               <td>{{ processor.id }}</td>
-              <td>{{ processor.registration_name }}</td>
-              <td>{{ processor.registration_name }}</td>
+              <td>{{ processor.url }}</td>
             </tr>
           </tbody>
         </v-table>
