b5a29531d15d283c05010fb544b62b275921c8e8
Vincent <courtois.vincent@outlook.com>
2024-01-18 12:46:15 +0100

    Display the nodes of a tool in the designated interface

---
+++ components/tools/Graph/Node.vue
@@ -0,0 +1,8 @@
+<template>
+  <rect :x="node.x" :y="node.y" fill="white" width="50" height="50" />
+</template>
+
+<script lang="ts" setup>
+import InnerAudioNode from '~~/lib/wrappers/InnerAudioNode';
+const { node } = defineProps<{node: InnerAudioNode}>();
+</script>
