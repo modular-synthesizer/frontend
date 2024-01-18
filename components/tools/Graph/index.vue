f6038a7a2e97155cf78473711688bb8133b75aa5
Vincent <courtois.vincent@outlook.com>
2024-01-18 11:24:34 +0100

    Add an SVG playground to put the nodes in

--- components/tools/Graph/index.vue
+++ components/tools/Graph/index.vue
@@ -1,6 +1,13 @@
 <template>
-  Pouet
+  <svg class="tool-graph"></svg>
 </template>
 
 <script lang="ts" setup>
 </script>
+
+<style scoped>
+svg.tool-graph {
+  height: calc(100vh - 150px);
+  width: 100%;
+}
+</style>
