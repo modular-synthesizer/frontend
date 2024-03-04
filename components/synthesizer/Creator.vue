79e60814961f52031b215190460e89aa1fc7afb8
Vincent <courtois.vincent@outlook.com>
2024-03-04 10:23:22 +0100

    Put the creation of a synthesizer in a dedicated component with validations

---
+++ components/synthesizer/Creator.vue
@@ -0,0 +1,118 @@
+<template>
+  <v-dialog max-width="500">
+    <template v-slot:activator="{ props: activatorProps }">
+      <v-layout-item model-value position="bottom" class="text-end pointer-events-none" size="88">
+        <div class="ma-4 pointer-events-initial">
+          <v-btn v-bind="activatorProps" color="primary" class="text-white" icon="mdi-plus" size="large" elevation="8" />
+        </div>
+      </v-layout-item>
+    </template>
+
+    <template v-slot:default=" { isActive }">
+      <v-card title="Créer un synthétiseur">
+        <v-form @submit.prevent="submit(isActive)" class="mt-5" validate-on="submit" ref="form">
+          <v-card-text>
+            <v-text-field
+              v-model="synthesizer.name"
+              variant="outlined"
+              :rules="rules.name"
+              :label="$t('synthesizers.create.labels.name')"
+              required
+              class="mb-3"
+            />
+            <v-text-field
+              v-model="synthesizer.racks"
+              type="number"
+              variant="outlined"
+              density="comfortable"
+              :label="$t('synthesizers.create.labels.racks')"
+              :min=1
+              :max=4
+              class="mb-3"
+            />
+            <v-text-field
+              v-model="synthesizer.slots"
+              type="number"
+              variant="outlined"
+              density="comfortable"
+              :label="$t('synthesizers.create.labels.slots')"
+              :min=10
+              :max=50
+              class="mb-3"
+            />
+            <v-text-field
+              v-model="synthesizer.voices"
+              type="number"
+              variant="outlined"
+              density="comfortable"
+              :label="$t('synthesizers.create.labels.voices')"
+              :min=1
+              :max=32
+              class="mb-3"
+              :rules="rules.voices"
+            />
+          </v-card-text>
+          <v-card-actions>
+            <v-btn type="submit" color="primary">{{ $t('common.create') }}</v-btn>
+            <v-spacer />
+            <v-btn @click="close(isActive)">{{ $t('common.close') }}</v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { boundaries, minlength, required } from '@/lib/rules';
+import { useI18n } from 'vue-i18n';
+import Synthesizer from '~~/lib/wrappers/Synthesizer';
+
+let synthesizer = ref(createEmptySynthesizer());
+
+const translator = useI18n();
+
+const synthesizers = ref(await useLists().synthesizers);
+
+const form = ref(null);
+
+const rules: {[key: string]: Function[]} = {
+  name: [
+    required("synthesizers.errors.name.required", translator),
+    minlength(6, "synthesizers.errors.name.minlength", translator),
+  ],
+  racks: [
+    boundaries(1, 4, "synthesizers.errors.racks.boundaries", translator),
+  ],
+  slots: [
+    boundaries(10, 50, "synthesizers.errors.slots.boundaries", translator),
+  ],
+  voices: [
+    boundaries(1, 32, "synthesizers.errors.voices.boundaries", translator),
+  ],
+};
+
+function close(isActive: Ref<boolean>) {
+  synthesizer = ref(createEmptySynthesizer());
+  isActive.value = false;
+}
+
+async function submit(isActive: Ref<boolean>) {
+  await form.value.validate();
+  if (form.value.modelValue) {
+    const creation: Synthesizer = new Synthesizer(synthesizer.value);
+    synthesizers.value.create(creation);
+    close(isActive)
+  }
+}
+</script>
+
+<style scoped>
+  .pointer-events-none {
+    pointer-events: none;
+  }
+  
+  .pointer-events-initial {
+    pointer-events: initial;
+  }
+</style>
--- pages/synthesizers/index.vue
+++ pages/synthesizers/index.vue
@@ -1,71 +1,25 @@
 <template>
-  <div>
-    <v-form @submit.prevent="submit" class="mt-5">
-      <v-container>
-        <v-row>
-          <v-col cols="3">
-            <v-text-field
-              v-model="synthesizer.name"
-              variant="outlined"
-              density="comfortable"
-              :label="$t('synthesizers.create.labels.name')"
-            />
-          </v-col>
-          <v-col cols="2">
-            <v-text-field
-              v-model="synthesizer.racks"
-              type="number"
-              variant="outlined"
-              density="comfortable"
-              :label="$t('synthesizers.create.labels.racks')"
-              :min="1"
-            />
-          </v-col>
-          <v-col cols="2">
-            <v-text-field
-              v-model="synthesizer.slots"
-              type="number"
-              variant="outlined"
-              density="comfortable"
-              :label="$t('synthesizers.create.labels.slots')"
-              :min=20
-            />
-          </v-col>
-          <v-col cols="2">
-            <v-text-field
-              v-model="synthesizer.voices"
-              type="number"
-              variant="outlined"
-              density="comfortable"
-              :label="$t('synthesizers.create.labels.voices')"
-              :min=1
-            />
-          </v-col>
-          <v-col cols="2">
-            <v-btn type="submit" size="large">{{ $t('common.create') }}</v-btn>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-form>
-    <v-container>
-      <v-row v-if="owned.length">
-        <v-col cols="12">
-          <div class="text-h5">{{ $t('synthesizers.list.owned') }}</div>
-        </v-col>
-        <v-col cols="12" sm="6" md="4" lg="3" v-for="synth in owned">
-          <synthesizer-card :synthesizer="synth" />
-        </v-col>
-      </v-row>
-      <v-row v-if="others.length">
-        <v-col cols="12">
-          <div class="text-h5">{{ $t('synthesizers.list.others') }}</div>
-        </v-col>
-        <v-col cols="12" sm="6" md="4" lg="3" v-for="synth in others">
-          <synthesizer-card :synthesizer="synth" />
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12"><synthesizer-creator /></v-col>
+    </v-row>
+    <v-row v-if="owned.length">
+      <v-col cols="12">
+        <div class="text-h5">{{ $t('synthesizers.list.owned') }}</div>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="3" v-for="synth in owned">
+        <synthesizer-card :synthesizer="synth" />
+      </v-col>
+    </v-row>
+    <v-row v-if="others.length">
+      <v-col cols="12">
+        <div class="text-h5">{{ $t('synthesizers.list.others') }}</div>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="3" v-for="synth in others">
+        <synthesizer-card :synthesizer="synth" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
@@ -83,9 +37,4 @@ function isOwned(s: Synthesizer) {
 const owned = computed(() => synthesizers.value.filter(isOwned))
 const others = computed(() => synthesizers.value.exclude(isOwned))
 const synthesizer = ref(createEmptySynthesizer());
-
-function submit() {
-  const creation: Synthesizer = new Synthesizer(synthesizer.value);
-  synthesizers.value.create(creation);
-}
 </script>
