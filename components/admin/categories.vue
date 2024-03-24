f907ad084e0b90000553f4cd02d44fd1853395e6
Vincent <courtois.vincent@outlook.com>
2024-03-24 11:02:57 +0100

    Add the new data fetcher to the administration panel

--- components/admin/categories.vue
+++ components/admin/categories.vue
@@ -1,50 +1,53 @@
 <template>
-  <v-container class="mt-2">
-    <v-row>
-      <v-col xs="12" sm="8" offset-sm="2" md="6" offset-md="3">
-        <form @submit.prevent="categories.create(category)">
-          <v-text-field
-            density="compact"
-            variant="outlined"
-            v-model="category.name"
-            :label="$t('categories.labels.name')"
-            append-inner-icon="mdi-plus"
-            @click:append-inner="categories.create(category)"
-            :hint="$t('categories.hints.name')"
-          />
-        </form>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12">
-        <v-table>
-          <thead>
-            <tr>
-              <th>{{ $t('common.uuid') }}</th>
-              <th>{{ $t('common.name') }}</th>
-              <th>&nbsp;</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="category in categories.all()">
-              <td>{{ category.id }}</td>
-              <td>{{ category.name }}</td>
-              <td>
-                <v-btn icon size="small" variant="text" @click="categories.delete(category.id)">
-                  <v-icon>mdi-delete</v-icon>
-                </v-btn>
-              </td>
-            </tr>
-          </tbody>
-        </v-Table>
-      </v-col>
-    </v-row>
-  </v-container>
+  <data-fetcher url="/categories">
+    <template v-slot="{ items: categories, add, remove }">
+      <v-container class="mt-2">
+        <v-row>
+          <v-col xs="12" sm="8" offset-sm="2" md="6" offset-md="3">
+            <form @submit.prevent="add(category)">
+              <v-text-field
+                density="compact"
+                variant="outlined"
+                v-model="category.name"
+                :label="$t('categories.labels.name')"
+                append-inner-icon="mdi-plus"
+                @click:append-inner="add(category)"
+                :hint="$t('categories.hints.name')"
+              />
+            </form>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <v-table>
+              <thead>
+                <tr>
+                  <th>{{ $t('common.uuid') }}</th>
+                  <th>{{ $t('common.name') }}</th>
+                  <th>&nbsp;</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="category in categories">
+                  <td>{{ category.id }}</td>
+                  <td>{{ category.name }}</td>
+                  <td>
+                    <v-btn icon size="small" variant="text" @click="remove(category.id)">
+                      <v-icon>mdi-delete</v-icon>
+                    </v-btn>
+                  </td>
+                </tr>
+              </tbody>
+            </v-Table>
+          </v-col>
+        </v-row>
+      </v-container>
+    </template>
+  </data-fetcher>
 </template>
 
 <script lang="ts" setup>
 import ICategory from '~~/lib/interfaces/ICategory';
 
-const categories = ref(await useLists().categories);
 const category: Ref<ICategory> = ref({ id: "", name: "" });
 </script>
--- components/admin/rights.vue
+++ components/admin/rights.vue
@@ -1,27 +1,27 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col xs="12" sm="8" offset-sm="2" md="6" offset-md="3">
-        <SoloFieldForm v-model="right.label" @submitted="rights.create($event)" />
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col xs="12">
-        <RightsTable v-slot="{ right }" :rights="rights.all()">
-          <v-btn icon small variant="plain" @click="rights.delete(right)">
-            <v-icon>mdi-delete</v-icon>
-          </v-btn>
-        </RightsTable>
-      </v-col>
-    </v-row>
-  </v-container>
+  <data-fetcher url="/rights">
+    <template v-slot="{ items: rights, remove, add }">
+      <v-container>
+        <v-row>
+          <v-col xs="12" sm="8" offset-sm="2" md="6" offset-md="3">
+            <SoloFieldForm v-model="right.label" @submitted="add" />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col xs="12">
+            <rights-table v-slot="{ right }" :rights="rights">
+              <v-btn icon small variant="plain" @click="remove(right)">
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </rights-table>
+          </v-col>
+        </v-row>
+      </v-container>
+    </template>
+  </data-fetcher>
 </template>
 
 <script lang="ts" setup>
-import IRight from '~~/lib/interfaces/permissions/IRight';
 import SoloFieldForm from '../common/SoloFieldForm.vue';
 import RightsTable from './utils/RightsTable.vue';
-
-const rights = ref(await useLists().rights);
-const right: IRight = { id: "", label: "" };
 </script>
