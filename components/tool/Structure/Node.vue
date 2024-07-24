5777b1578278c56b798982a8ca5a361b851d6faf
Vincent <courtois.vincent@outlook.com>
2024-07-24 16:40:23 +0200

    Add the possibility to move a node with the arrow keys when selected

--- components/tool/Structure/Node.vue
+++ components/tool/Structure/Node.vue
@@ -1,17 +1,40 @@
 <template>
-  <g :transform="`translate(${node.x} ${node.y})`" @click="emit('select', node)">
+  <g :transform="`translate(${coords.x} ${coords.y})`" @click="emit('select', node)" @keydown="handleKeyDown">
     <rect height="60" width="60" fill="white" />
-    <rect height="60" width="60" fill="transparent" stroke="red" stroke-width="3" v-if="selected" />
+    <rect height="60" width="60" fill="transparent" stroke="red" stroke-width="3" v-if="props.selected" />
   </g>
 </template>
 
 <script setup lang="ts">
+import { PropType } from 'vue';
 import { InnerNode } from '~~/lib/interfaces/ITool';
 
-const { node } = defineProps({
+const props = defineProps({
   node: { type: Object as PropType<InnerNode>, required: true },
   selected: { type: Boolean, default: false}
 })
 
-const emit = defineEmits<{ select: [item: InnerNode]}>();
+const coords: Ref<ICoordinates> = ref({ x: props.node.x, y: props.node.y })
+
+const emit = defineEmits<{
+  select: [item: InnerNode],
+}>();
+
+window.addEventListener('keydown', (event: KeyboardEvent) => {
+  if (!props.selected) return;
+  switch(event.code) {
+    case 'ArrowRight':
+      coords.value.x += 20
+      break;
+    case 'ArrowLeft':
+      coords.value.x -= 20
+      break;
+    case 'ArrowDown':
+      coords.value.y += 20
+      break;
+    case 'ArrowUp':
+      coords.value.y -= 20
+      break;
+  }
+});
 </script>
