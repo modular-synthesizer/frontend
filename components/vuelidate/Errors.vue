d782259109a2dbb5af3d428a7fdf12ae99f9a252
Vincent <courtois.vincent@outlook.com>
2024-10-03 15:22:30 +0200

    Avoid having the same error displayed multiple times

--- components/vuelidate/Errors.vue
+++ components/vuelidate/Errors.vue
@@ -1,24 +1,24 @@
 <template>
   <v-alert type="error" variant="outlined">
     <v-alert-title>Erreurs dans le formulaire</v-alert-title>
-    <div v-for="error in errors">
-      <template v-if="error.$validator === '$externalResults'">
-        {{ $t(`errors.${error.$property}.${error.$message}`) }}
-      </template>
-      <template v-else>
-        {{ $t(`errors.${error.$property}.${error.$validator}`) }}
-      </template>
-    </div>
+    <div v-for="error in formattedErrors()">{{ $t(error) }}</div>
   </v-alert>
 </template>
 
-<script lang="ts">
-export default {
-  props: {
-    errors: {
-      type: Array<any>,
-      default: () => []
+<script lang="ts" setup>
+import { map, uniq } from 'lodash';
+
+const props = defineProps({ errors: { type: Array<any>, default: () => [] }});
+
+function formattedErrors() {
+  const errors: string[] = map(props.errors, (error: any) => {
+    if (error.$validator === '$externalResults') {
+      return `errors.${error.$property}.${error.$message}`
     }
-  }
+    else {
+      return `errors.${error.$property}.${error.$validator}`
+    }
+  });
+  return uniq(errors);
 }
 </script>
