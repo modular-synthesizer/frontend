1943770a3cead7b0d7cda0d00dcaf75387a2af9c
Vincent <courtois.vincent@outlook.com>
2024-11-17 16:48:07 +0100

    Map the new tool view on the old route

--- pages/tools/[id].vue
+++ pages/tools/[id].vue
@@ -1,18 +1,48 @@
 <template>
-  <template v-if="tool !== null">
-    <tools-creator v-model="tool" @update:model-value="save" />
-  </template>
+  <v-container fluid class="main-wrapper">
+  <v-tabs v-model="tab" align-tabs="center" color="secondary">
+    <v-tab value="internal">Internal structure</v-tab>
+    <v-tab value="external">External appearance</v-tab>
+  </v-tabs>
+  <div class="wrapper">
+  </div>
+    <div class="pt-2" v-if="tool">
+      <v-window v-model="tab">
+        <v-window-item value="internal">
+          <div class="tabs-holder"><tool-structure :tool="tool" /></div>
+        </v-window-item>
+        <v-window-item value="external">
+          <div class="tabs-holder"><tool-appearance :tool="tool" /></div>
+        </v-window-item>
+      </v-window>
+    </div>
+  </v-container>
 </template>
 
-<script lang="ts">
-import ITool from "~~/lib/interfaces/ITool";
-import { pick } from 'lodash';
-import { repositories } from "~~/lib/repositories";
+<script lang="ts" setup>
+import ITool from '~~/lib/interfaces/ITool';
+import { repositories } from '~~/lib/repositories';
 
+definePageMeta({ layout: 'empty' });
+const tab: Ref<string> = ref('internal');
 const tool: Ref<ITool> = ref(await repositories.tools.get(useRoute().params.id as string));
-
-function save() {
-  const fields: string[] = ['id', 'name', 'slots', 'categoryId', 'experimental'];
-  repositories.tools.update(pick(tool.value, fields) as ITool);
-}
 </script>
+
+<style scoped>
+.main-wrapper {
+  padding: 0px 20px;
+}
+.tabs-holder {
+  height: calc(100vh - 56px);
+  overflow-y: hidden;
+}
+.wrapper {
+  height: 100vh;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+}
+.menu-wrapper {
+  position: absolute;
+}
+</style>
