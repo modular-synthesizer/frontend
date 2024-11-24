063722c7ef93b459d4825b5d0c7db311ef09edcc
Vincent <courtois.vincent@outlook.com>
2024-11-24 22:41:06 +0100

    Fix the midi composable

--- composables/parameters/useMidiLearn.ts
+++ composables/parameters/useMidiLearn.ts
@@ -14,6 +14,30 @@ const state: Ref<State> = useState<State>('useMidiLearn', () => ({
 }))
 
 export function useMidiLearn() {
+  function bindMidiLearn(k: number, parameter: Parameter) {
+    if (!(parameter.id in state.value.handlers)) state.value.handlers[parameter.id] = {};
+
+    state.value.handlers[parameter.id][k] = messageHandler(parameter);
+    eventbus.subscribe(`midi/generalpurpose/${k}`, state.value.handlers[parameter.id][k]);
+  }
+  function messageHandler(parameter: Parameter) {
+    return ({ amount }: any) => {
+      if (state.value.timeout !== -1) window.clearTimeout(state.value.timeout);
+      const ratio: number = amount / 127;
+      const gap: number = parameter.maximum - parameter.minimum;
+      
+      const value: number = parameter.minimum + (gap * ratio);
+      const step: number = parameter.step * 10
+      const flooredValue: number = Math.floor(value / step) * step
+      parameter.setValue(flooredValue);
+      
+      state.value.timeout = window.setTimeout(() => {
+        saveParameter(parameter);
+        sendParamEvent('endEdit', parameter);
+        state.value.timeout = -1;
+      }, 250);
+    }
+  }
   return {
     state,
     get handlers(): Handlers { return state.value.handlers; },
@@ -21,34 +45,10 @@ export function useMidiLearn() {
     learn(parameter: Parameter) {
       const fct = ({ knob }: any) => {
         eventbus.unsubscribe("midi/generalpurpose", fct);
-        this.bindMidiLearn(knob, parameter)
+        bindMidiLearn(knob, parameter)
       }
       useContexts().hide();
       eventbus.subscribe("midi/generalpurpose", fct)
-    },
-    bindMidiLearn(k: number, parameter: Parameter) {
-      if (!(parameter.id in this.handlers)) this.handlers[parameter.id] = {};
-
-      this.handlers[parameter.id][k] = this.messageHandler(parameter);
-      eventbus.subscribe(`midi/generalpurpose/${k}`, this.handlers[parameter.id][k]);
-    },
-    messageHandler(parameter: Parameter) {
-      return ({ amount }: any) => {
-        if (this.timeout !== -1) window.clearTimeout(this.timeout);
-        const ratio: number = amount / 127;
-        const gap: number = parameter.maximum - parameter.minimum;
-        
-        const value: number = parameter.minimum + (gap * ratio);
-        const step: number = parameter.step * 10
-        const flooredValue: number = Math.floor(value / step) * step
-        parameter.setValue(flooredValue);
-        
-        state.value.timeout = window.setTimeout(() => {
-          saveParameter(parameter);
-          sendParamEvent('endEdit', parameter);
-          state.value.timeout = -1;
-        }, 250);
-      }
     },
     unlearn(parameter: Parameter) {
       if(!(parameter.id in this.handlers)) return;
@@ -57,6 +57,7 @@ export function useMidiLearn() {
         eventbus.unsubscribe(`midi/generalpurpose/${k}`, this.handlers[parameter.id][k]);
         delete this.handlers[parameter.id][k];
       })
-    }
+    },
+    messageHandler,
   }
 }
