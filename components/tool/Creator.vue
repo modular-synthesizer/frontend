1cc2aa40d8453aae6c81e33c5c6065fd22ca0182
Vincent Courtois <1634362+vincentcourtois@users.noreply.github.com>
2024-11-24 01:04:38 +0100

    Change the way the tool is updated after updating internal elements (#107)

--- components/tool/Creator.vue
+++ components/tool/Creator.vue
@@ -1,12 +1,12 @@
 <template>
-  <tool-menu :tool="props.tool" @mode-changed="changeMode" @save="onSaveRequest" :creation-mode="createMode" />
-  <div class="global-wrapper" v-if="props.tool">
+  <tool-menu :tool="tool" @mode-changed="changeMode" @save="onSaveRequest" :creation-mode="createMode" />
+  <div class="global-wrapper" v-if="modelValue">
     <div>{{ createMode }}</div>
     <v-form v-model="valid" ref="form" v-if="mode === 'infos'" @submit.prevent.stop>
-      <tools-informations v-model="props.tool" :rules="rules" />
+      <tools-informations v-model="tool" :rules="rules" />
     </v-form>
-    <tool-structure v-else-if="mode === 'structure'" :tool="props.tool" />
-    <tool-appearance v-else :tool="props.tool" :creation-mode="createMode" />
+    <tool-structure v-else-if="mode === 'structure'" :tool="tool" />
+    <tool-appearance v-else :tool="tool" :creation-mode="createMode" />
   </div>
 </template>
 
@@ -18,10 +18,11 @@ import { required } from '~~/lib/rules';
 import { ToolTabs } from '~~/lib/types/ToolTabs';
 
 const props = defineProps({
-  tool: { type: Object as PropType<ITool>, required: true },
+  modelValue: { type: Object as PropType<ITool>, required: true },
   creationMode: { type: Boolean, default: false },
 });
 
+const tool = ref(props.modelValue);
 const mode: Ref<ToolTabs> = ref('infos');
 const valid: Ref<boolean|null> = ref(null);
 const form = ref<HTMLFormElement|null>(null);
@@ -35,18 +36,20 @@ function changeMode(m: ToolTabs) {
   mode.value = m;
 }
 
-async function onSaveRequest() {
+async function onSaveRequest(t: ITool) {
+  console.log(t);
+  tool.value = t;
   await form.value?.validate();
   if (valid.value) save();
 }
 
 async function save() {
   if (props.creationMode) {
-    const creation: ITool = await repositories.tools.create(props.tool);
-    props.tool.id = creation.id;
+    const creation: ITool = await repositories.tools.create(props.modelValue);
+    props.modelValue.id = creation.id;
     createMode.value = false;
   }
-  else repositories.tools.update(props.tool);
+  else repositories.tools.update(props.modelValue);
 }
 </script>
 
