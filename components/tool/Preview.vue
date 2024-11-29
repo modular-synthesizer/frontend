8eba1a1752c05695cbb24933dc043d6abb5a0da0
Vincent <courtois.vincent@outlook.com>
2024-11-29 21:39:42 +0100

    Add a state to handle control edition properly

--- components/tool/Preview.vue
+++ components/tool/Preview.vue
@@ -7,16 +7,17 @@
         <g
           v-for="control in tool.controls"
           @mousedown.left.capture.stop="useControlSelection().selectControl(control)"
-          @dblclick.capture.stop="startEdit(control)"
+          @dblclick.capture.stop="useControlEdition().startEdit(control)"
         >
           <controls-wrapper :mod="mod" :control="control" />
         </g>
       </g>
     </template>
   </draggable-tool-stage>
-  <v-dialog max-width="500" v-model="displayEdition">
+  <v-dialog max-width="500" v-model="useControlEdition().state.value.displayed">
     <v-card>
-      {{ edited?.id }}
+      <v-card-title>{{ useControlEdition().state.value.creating ? 'creation' : 'edition' }}</v-card-title>
+      {{ useControlEdition().state.value.control?.id }}
     </v-card>
   </v-dialog>
 </template>
@@ -24,12 +25,12 @@
 <script setup lang="ts">
 import { PropType } from 'vue';
 import ModulesFactory from '~~/lib/factories/ModulesFactory';
-import { IControl } from '~~/lib/interfaces/IControl';
 import ITool from '~~/lib/interfaces/ITool';
 import IModule from '~~/lib/interfaces/modules/IModule';
 import { ScalablePosition } from '~~/lib/types/ScalablePosition';
 import { RACK_HEIGHT, SLOT_SIZE } from '~~/lib/utils/constants';
 import { FakeModule } from '~~/lib/wrappers/FakeModule';
+
 const props = defineProps({
   modelValue: { type: Object as PropType<ITool>, required: true },
 });
@@ -42,11 +43,4 @@ const moveMode: Ref<boolean> = ref(false);
 
 const modHeight: number = RACK_HEIGHT;
 const modWidth: number = SLOT_SIZE * tool.value.slots;
-
-const displayEdition = ref(false);
-const edited: Ref<IControl | undefined> = ref(undefined);
-function startEdit(control: IControl) {
-  displayEdition.value = true;
-  edited.value = control
-}
 </script>
---
+++ composables/controls/useControlEdition.ts
@@ -0,0 +1,23 @@
+import { IControl } from "~~/lib/interfaces/IControl";
+
+type State = {
+  control?: IControl,
+  displayed: boolean,
+  get creating(): boolean,
+}
+
+const state: Ref<State> = ref({
+  displayed: false,
+  get creating(): boolean {
+    return state.value.control?.id !== '';
+  }
+});
+
+function startEdit(control: IControl) {
+  state.value.control = control;
+  state.value.displayed = true;
+}
+
+export function useControlEdition() {
+  return { startEdit, state };
+}
