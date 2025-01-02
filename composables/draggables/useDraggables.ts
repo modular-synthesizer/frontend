0b5dd94c2bb7fa616a96736bcf12fb2884b2d0b3
Vincent <courtois.vincent@outlook.com>
2025-01-02 11:05:05 +0100

    Rewrite the array of stages and draggables as maps to allow lean syntax

--- composables/draggables/useDraggables.ts
+++ composables/draggables/useDraggables.ts
@@ -5,13 +5,12 @@ import { ItemDragStrategy } from "./ItemDragStrategy"
 import { StagePanStrategy } from "./StagePanStrategy"
 
 export type Stage = {
-  name: string,
   // The offset coordinates of the stage INSIDE (how is it moved from its 0;0 point).
   d: Coordinates,
   // The offset coordinates of the stage OUTSIDE (where is it on the screen currently).
   a: Coordinates,
   scale: number
-  draggables: Array<Draggable>,
+  draggables: Record<string, Draggable>,
   strategy?: DragStrategy,
 }
 
@@ -21,17 +20,16 @@ type DraggableOptions = {
 }
 
 export type Draggable = {
-  name: string,
   item: Coordinates,
   options: DraggableOptions,
   stage: Stage,
 }
 
 type State = {
-  stages: Array<Stage>,
+  stages: Record<string, Stage>
 }
 
-const state: Ref<State> = ref({ stages: [] });
+const state: Ref<State> = ref({ stages: {} });
 
 /**
  * 
@@ -42,36 +40,25 @@ const state: Ref<State> = ref({ stages: [] });
  * @param options Some configuration options, like the number of pixels of a drag step, etc.
  */
 function declare(name: string, d: Coordinates = { x: 0, y: 0 }, a: Coordinates = { x: 0, y: 0 }, scale: number = 1): Stage {
-  const stage: Stage = { name, d, a, scale, draggables: [] };
-  state.value.stages.push(stage);
+  const stage: Stage = { d, a, scale, draggables: { } };
+  state.value.stages[name] = stage;
   return stage;
 }
 
 function addDraggable(stageName: string, name: string, item: Coordinates = { x: 0, y: 0 }, options: DraggableOptions = { sx: 1, sy: 1 }): Draggable {
   let stage = getStage(stageName);
-  const draggable: Draggable = { name, item, options, stage }
-  getStage(stageName).draggables.push(draggable);
+  const draggable: Draggable = { item, options, stage }
+  getStage(stageName).draggables[name] = draggable;
   return draggable;
 }
 
 function getStage(name: string): Stage {
-  const found: Stage | undefined = state.value.stages.find((s: Stage) => s.name === name);
-  if (found === undefined) {
-    const stage: Stage = { name, d: { x: 0, y: 0 }, a: { x: 0, y: 0 }, scale: 1, draggables: [ ] };
-    state.value.stages.push(stage);
-    return stage;
-  }
-  return found;
-}
-
-function getDraggable(stage: Stage, name: string): Draggable {
-  const found: Draggable | undefined = stage.draggables.find((d: Draggable) => d.name === name);
-  return found ?? addDraggable(stage.name, name, { x: 0, y: 0 }, { sx: 0, sy: 0 });
+  return state.value.stages[name];
 }
 
 function startDrag(stageName: string, name: string, $event: MouseEvent) {
   const stage: Stage = getStage(stageName);
-  const draggable = getDraggable(stage, name);
+  const draggable = stage.draggables[name];
 
   stage.strategy = new ItemDragStrategy(stage, draggable.item, 5, 5);
   stage.strategy.start($event);
