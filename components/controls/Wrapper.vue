4c8d3c5a984b513acad9afc8894334a683b1d977
Vincent <courtois.vincent@outlook.com>
2025-01-17 18:32:05 +0100

    Format the wrapper component

--- components/controls/Wrapper.vue
+++ components/controls/Wrapper.vue
@@ -7,14 +7,8 @@ import type { AudioModule } from "~/types/modules/AudioModule";
 import type { Control } from "~/types/tools/Control";
 
 const { control, mod } = defineProps({
-  control: {
-    type: Object as PropType<Control>,
-    required: true,
-  },
-  mod: {
-    type: Object as PropType<AudioModule>,
-    required: true,
-  }
+  control: { type: Object as PropType<Control>, required: true, },
+  mod: { type: Object as PropType<AudioModule>, required: true, }
 });
 
 const component = await import("./" + control.component + ".vue");
