4e1fe77d6a04a5c2a6938678324f063e54d693a0
Vincent <courtois.vincent@outlook.com>
2025-01-23 16:17:49 +0100

    Add the last of the controls to the new folder

--- components/control/index.ts
+++ components/control/index.ts
@@ -2,5 +2,8 @@ import Port  from "./Port.vue";
 import SmallKnob  from "./SmallKnob.vue";
 import Knob  from "./Knob.vue";
 import LargeKnob  from "./LargeKnob.vue";
+import Fader  from "./Fader.vue";
+import MidiController  from "./MidiController.vue";
+import Oscilloscope from "./oscilloscope/Index.vue"
 
-export { LargeKnob, Knob, Port, SmallKnob }
+export { Fader, Knob, LargeKnob, MidiController, Oscilloscope, Port, SmallKnob }
--- components/controls/index.ts
+++
@@ -1,7 +0,0 @@
-import Fader from "./Fader.vue";
-import MidiController from "./MidiController.vue";
-import Oscilloscope from "./Oscilloscope.vue";
-
-export const components: Record<string, any> = {
-  Oscilloscope, MidiController, Fader
-}
